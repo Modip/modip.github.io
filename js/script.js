69e41b43b7ea52f90dadacfb1a6114da924e9112
modip <mordip06@mail.com>
2022-12-14 23:15:58 +0000

    some changes

--- js/script.js
+++ js/script.js
@@ -1,62 +1,15 @@
+const menubtn=document.getElementById("menu-btn") ;
+const sidemenu=document.getElementById("sidemenu") ;
 
-        $(document).ready(function(){
-            $('#menu-btn').click(function(){
-                $('ul').toggleClass('show');
-                $('.menu-btn i').toggleClass("active");
-            })
-        });
-
-
-let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+menubtn.onclick = function(){
+    menubtn.classList.toggle('active');
+    sidemenu.classList.toggle('active');
 }
 
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
-
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname){
-    for (tablink of tablinks){
-        tablink.classList.remove("active-link");
+document.onclick = function(clickEvent){
+    if(clickEvent.target.id !== "menu-btn" && clickEvent.target.id !== "sidemenu"){
+       
+        menubtn.classList.toggle('active'); 
+        sidemenu.classList.toggle('active');
     }
-    for (tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-
 }
-
-navbar = document.querySelector(".navbar").querySelectorAll("a");
-console.log(navbar);
-
-navbar.forEach(element=>{
-    element.addEventListener("click", function(){
-        navbar.forEach(nav=>nav.classList.remove("active"))
-        this.classList.add("active");
-    })
-});
